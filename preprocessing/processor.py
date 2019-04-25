4b4d9e22c9092f64a6ff2ab447816f2a4d036e10
RaayChen <leichencmu@gmail.com>
2019-04-25 01:40:47 -0400

    better token join

--- preprocessing/processor.py
+++ preprocessing/processor.py
@@ -6,44 +6,62 @@ import nltk
 
 lemmatizer = nltk.wordnet.WordNetLemmatizer()
 
+
 def process_intent(intent):
     intent, slot_map = tokenize_intent(intent)
     intent = [lemmatizer.lemmatize(e.lower()) for e in intent]
     return intent, slot_map
-    
+
+
 def replace_value(string):
     return string.replace('\\', '\\\\').replace('\t', '\\t').replace('\n', '\\n').replace('\r', '\\r')
+
 
 def sub_slotmap(tokens, slot_map):
     for i in range(len(tokens)):
         if tokens[i] in slot_map:
             value = slot_map[tokens[i]]['value']
             tokens[i] = replace_value(value)
-            
+
         elif len(tokens[i]) > 2 and tokens[i][1:-1] in slot_map:
             slot = slot_map[tokens[i][1:-1]]
             quote = tokens[i][0]
             value = slot['value']
             tokens[i] = quote + replace_value(value) + quote
-            
+
         elif len(tokens[i]) > 6 and tokens[i][3:-3] in slot_map:
             slot = slot_map[tokens[i][3:-3]]
             quote = tokens[i][0:3]
             value = slot['value']
             tokens[i] = quote + replace_value(value) + quote
-            
-    return ' '.join(tokens)
+    res = ''
+    prev = ''
+    non_space_set = {'.', '(', ')'}
+    for token in tokens:
+        if prev is ')' and token not in non_space_set:
+            res += ' ' + token
+        elif token in non_space_set or prev in non_space_set:
+            res += token
+        elif token is '.' or prev is '.':
+            res += token
+        else:
+            res += ' ' + token
+        prev = token
+    return res
+
 
 def tokenize_conala_entry(entry):
     intent, slot_map = process_intent(entry['intent'])
     code = tokenize_code(entry['code'], slot_map)
     return intent, code, slot_map
 
+
 def get_raw_entries(path=None):
     if path == None:
         path = '../corpus/train.json'
     with open(path) as f:
         return json.load(f)
+
 
 ### Handle all pre/post-processing of Code-Intent Pair
 class Code_Intent_Pairs():
@@ -53,15 +71,15 @@ class Code_Intent_Pairs():
         self.word2num = None
         self.code2num = None
         self.entries = None
-        
+
     def __getitem__(self, idx: int):
         return self.entries[idx]
-    
+
     def intent2idx(self, intent):
         word_dict = self.word2num
         unk = word_dict['<unk>']
         return [word_dict[word] if word in word_dict else unk for word in intent]
-    
+
     def idx2intent(self, idxes):
         intent = []
         sos = self.word2num['<sos>']
@@ -71,7 +89,7 @@ class Code_Intent_Pairs():
             if idx not in (sos, eos, pad):
                 intent.append(self.num2word[idx])
         return intent
-    
+
     def code2idx(self, code, intent=None):
         code_dict = self.code2num
         if intent != None:
@@ -87,7 +105,7 @@ class Code_Intent_Pairs():
         else:
             unk = code_dict['<unk>']
             return [code_dict[token] if token in code_dict else unk for token in code]
-        
+
     def idx2code(self, idxes, intent=None):
         tokens = []
         sos = self.code2num['<sos>']
@@ -95,21 +113,21 @@ class Code_Intent_Pairs():
         pad = self.code2num['<pad>']
         num2code = self.num2code
         size = len(num2code)
-        if intent!=None:
+        if intent != None:
             for idx in idxes:
                 if idx < size:
                     if idx not in (sos, eos, pad):
                         tokens.append(num2code[idx])
                 else:
-                    tokens.append(intent[idx-size])
+                    tokens.append(intent[idx - size])
             return tokens
         else:
             return [num2code[idx] for idx in idxes]
-    
+
     def get_dict_from_raw(self, path=None, word_cut_freq=5, code_cut_freq=3, copy=True, store=True):
         raw_entries = get_raw_entries()
         intents, codes, slot_maps = zip(*list(map(tokenize_conala_entry, raw_entries)))
-        
+
         def get_vocab(sentences, cut_freq):
             vocab = Counter()
             for sentence in sentences:
@@ -119,18 +137,18 @@ class Code_Intent_Pairs():
             vocab = [k for k in vocab if vocab[k] >= cut_freq]
             vocab.extend(['<unk>', '<sos>', '<eos>', '<pad>'])
             return vocab
-        
+
         num2word = get_vocab(intents, word_cut_freq)
-        word2num = dict(zip(num2word, range(0,len(num2word))))
-        
+        word2num = dict(zip(num2word, range(0, len(num2word))))
+
         num2code = get_vocab(codes, code_cut_freq)
-        code2num = dict(zip(num2code, range(0,len(num2code))))
-        
+        code2num = dict(zip(num2code, range(0, len(num2code))))
+
         self.num2word = num2word
         self.word2num = word2num
         self.num2code = num2code
         self.code2num = code2num
-        
+
     def load_raw_data(self, path):
         raw_entries = get_raw_entries(path)
         entries = [tokenize_conala_entry(entry) for entry in raw_entries]
@@ -151,7 +169,7 @@ class Code_Intent_Pairs():
             }
             self.entries.append(entry_dict)
         return self.entries
-    
+
     def load_raw_test_data(self, path):
         raw_entries = get_raw_entries(path)
         entries = [tokenize_conala_entry(entry) for entry in raw_entries]
@@ -170,16 +188,16 @@ class Code_Intent_Pairs():
             }
             self.entries.append(entry_dict)
         return self.entries
-    
+
     def store_entries(self, path):
         with open(path, 'w') as f:
             json.dump(self.entries, f, indent=2)
-            
+
     def load_entries(self, path):
         with open(path) as f:
             self.entries = json.load(f)
         return self.entries
-            
+
     def store_dict(self, path=None):
         if path == None:
             path = '../vocab/'
@@ -187,17 +205,17 @@ class Code_Intent_Pairs():
         pickle.dump(self.num2word, open(word_dict_path, 'wb'))
         code_dict_path = path + 'code_dict.bin'
         pickle.dump(self.num2code, open(code_dict_path, 'wb'))
-        
+
     def load_dict(self, path=None):
         if path == None:
             path = '../vocab/'
         word_dict_path = path + 'word_dict.bin'
         self.num2word = pickle.load(open(word_dict_path, 'rb'))
-        self.word2num = dict(zip(self.num2word, range(0,len(self.num2word))))
+        self.word2num = dict(zip(self.num2word, range(0, len(self.num2word))))
         code_dict_path = path + 'code_dict.bin'
         self.num2code = pickle.load(open(code_dict_path, 'rb'))
-        self.code2num = dict(zip(self.num2code, range(0,len(self.num2code))))
-        
+        self.code2num = dict(zip(self.num2code, range(0, len(self.num2code))))
+
     def pad(self, pad_code=True, pad_intent=False):
         if not pad_code and not pad_intent:
             return self.entries
@@ -213,7 +231,7 @@ class Code_Intent_Pairs():
                 entry['code_indx_nocopy'] = [code_sos] + entry['code_indx_nocopy'] + [code_eos]
             if pad_intent:
                 entry['intent_indx'] = [intent_sos] + entry['intent_indx'] + [intent_eos]
-    
+
     def get_special_symbols(self):
         return {
             'word_pad': self.word2num['<pad>'],
@@ -225,15 +243,14 @@ class Code_Intent_Pairs():
             'code_eos': self.code2num['<eos>'],
             'code_unk': self.code2num['<unk>'],
         }
-        
+
     def get_word_size(self):
         return len(self.num2word)
-        
+
     def get_code_size(self):
         return len(self.num2code)
-            
-     
-    
+
+
 ### Handle code language model processing
 class Code():
     def __init__(self):
@@ -246,26 +263,26 @@ class Code():
             path = '../vocab/'
         code_dict_path = path + 'code_dict.bin'
         self.num2code = pickle.load(open(code_dict_path, 'rb'))
-        self.code2num = dict(zip(self.num2code, range(0,len(self.num2code))))
-        
+        self.code2num = dict(zip(self.num2code, range(0, len(self.num2code))))
+
     def get_special_symbols(self):
         return {
             'pad': self.code2num['<pad>'],
             'sos': self.code2num['<sos>'],
             'eos': self.code2num['<eos>'],
         }
-    
+
     def code2idx(self, code):
         code_dict = self.code2num
         unk = code_dict['<unk>']
         return [code_dict[token] if token in code_dict else unk for token in code]
-        
+
     def load_data(self, path):
         with open(path, 'r') as f:
             lines_token = [tokenize_code(line) for line in f]
         self.codes_indx = [self.code2idx(line_token) for line_token in lines_token]
         return self.codes_indx
-    
+
     def pad(self, pad_length=1):
         if pad_length <= 0:
             return self.code_indxes
@@ -273,7 +290,5 @@ class Code():
         eos = self.code2num['<eos>']
         for i in range(len(self.codes_indx)):
             self.codes_indx[i] = [sos] * pad_length \
-                + self.codes_indx[i] + [eos] * pad_length
+                                 + self.codes_indx[i] + [eos] * pad_length
         return self.codes_indx
-                
-    
